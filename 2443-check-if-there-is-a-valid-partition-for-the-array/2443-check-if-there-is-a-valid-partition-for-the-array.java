cc309705ec3189f0b977c4041aefae34d5b5d56b
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-13 12:11:00 +0530

    Time: 9 ms (68.93%), Space: 56.4 MB (89.32%) - LeetHub

--- 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
@@ -4,9 +4,12 @@ class Solution {
         int n = nums.length;
         boolean dp[] = new boolean[n];
         if (n == 1) return false;
+        //basecase
         dp[0] = false;
+        //check if first two elements are equal
         dp[1] = nums[1] == nums[0];
         for (int i = 2; i < n; i++) {
+            // check for all three conditions
             boolean v1 = isTwoElementsEqual(i, nums);
             boolean v2 = isThreeElementsEqual(i, nums);
             boolean v3 = isThreeConsEqual(i, nums);
@@ -17,7 +20,6 @@ class Solution {
                 dp[i] = dp[i] || ((i - 3) < 0 ? true : dp[i - 3]);
             }
         }
-        System.out.println(Arrays.toString(dp));
         return dp[n - 1];
     }
 
