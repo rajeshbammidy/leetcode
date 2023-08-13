7b50205b39108ec331670969b7ec45e514108e96
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-13 12:08:54 +0530

    Time: 50 ms (5.83%), Space: 57.3 MB (46.6%) - LeetHub

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        boolean dp[] = new boolean[n];
+        if (n == 1) return false;
+        dp[0] = false;
+        dp[1] = nums[1] == nums[0];
+        for (int i = 2; i < n; i++) {
+            boolean v1 = isTwoElementsEqual(i, nums);
+            boolean v2 = isThreeElementsEqual(i, nums);
+            boolean v3 = isThreeConsEqual(i, nums);
+            if (v1) {
+                dp[i] = dp[i - 2];
+            }
+            if (v2 || v3) {
+                dp[i] = dp[i] || ((i - 3) < 0 ? true : dp[i - 3]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n - 1];
+    }
+
+    boolean isTwoElementsEqual(int i, int[] nums) {
+        return nums[i] == nums[i - 1];
+    }
+
+    boolean isThreeElementsEqual(int i, int[] nums) {
+        return nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2];
+    }
+
+    boolean isThreeConsEqual(int i, int[] nums) {
+        return (nums[i] == (nums[i - 1] + 1)) && (nums[i - 1] == (nums[i - 2] + 1));
+    }
+}
