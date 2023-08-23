e53605198e70a7f1bbc5e287506a19a04e7e5cce
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-23 17:34:58 -0400

    Time: 8 ms (14.2%), Space: 41 MB (33.42%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,42 +1,53 @@
 class Solution {
 
+    /**
+    
+    Intution: You have to sort <key,value> wrt values and take the highest freq character first. 
+     */
+
     public String reorganizeString(String s) {
         if (s.isEmpty()) return "";
 
-        PriorityQueue<Pair<Character, Integer>> minHeap = new PriorityQueue<>(
+        // maxHeap to find the highest freq key
+        PriorityQueue<Pair<Character, Integer>> maxHeap = new PriorityQueue<>(
             new Comparator<Pair<Character, Integer>>() {
 
                 public int compare(Pair<Character, Integer> o1, Pair<Character, Integer> o2) {
-                   return o2.getValue().compareTo(o1.getValue());
+                    return o2.getValue().compareTo(o1.getValue());
                 }
             }
         );
+        // map to store the key and freq's
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         for (char ch : s.toCharArray()) {
             map.putIfAbsent(ch, 0);
             map.put(ch, map.get(ch) + 1);
         }
+        // Add the pairs to maxHeap
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            minHeap.add(new Pair<Character, Integer>(entry.getKey(), entry.getValue()));
+            maxHeap.add(new Pair<Character, Integer>(entry.getKey(), entry.getValue()));
         }
+
+        // to store the string
         StringBuilder sb = new StringBuilder();
 
-        while (!minHeap.isEmpty()) {
+        while (!maxHeap.isEmpty()) {
+            //if there is no prev character to compare
             if (sb.length() == 0) {
-                Pair<Character, Integer> pair = minHeap.poll();
+                Pair<Character, Integer> pair = maxHeap.poll();
                 sb.append(pair.getKey());
-                if (pair.getValue() > 1) minHeap.add(new Pair<Character, Integer>(pair.getKey(), pair.getValue() - 1));
+                if (pair.getValue() > 1) maxHeap.add(new Pair<Character, Integer>(pair.getKey(), pair.getValue() - 1));
             } else {
-                Pair<Character, Integer> pair1 = minHeap.poll();
+                Pair<Character, Integer> pair1 = maxHeap.poll();
                 if (sb.charAt(sb.length() - 1) == pair1.getKey()) {
-                    if (minHeap.isEmpty()) return "";
-                    Pair<Character, Integer> pair2 = minHeap.poll();
+                    if (maxHeap.isEmpty()) return "";
+                    Pair<Character, Integer> pair2 = maxHeap.poll();
                     sb.append(pair2.getKey());
-                    if (pair2.getValue() > 1) minHeap.add(new Pair<Character, Integer>(pair2.getKey(), pair2.getValue() - 1));
-                    minHeap.add(pair1);
+                    if (pair2.getValue() > 1) maxHeap.add(new Pair<Character, Integer>(pair2.getKey(), pair2.getValue() - 1));
+                    maxHeap.add(pair1);
                 } else {
                     sb.append(pair1.getKey());
-                    if (pair1.getValue() > 1) minHeap.add(new Pair<Character, Integer>(pair1.getKey(), pair1.getValue() - 1));
+                    if (pair1.getValue() > 1) maxHeap.add(new Pair<Character, Integer>(pair1.getKey(), pair1.getValue() - 1));
                 }
             }
         }
