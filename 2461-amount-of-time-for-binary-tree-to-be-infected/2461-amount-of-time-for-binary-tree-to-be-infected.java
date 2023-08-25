dce2f406075e33583683bc84d61673df5c0426ef
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-25 01:32:41 -0400

    Time: 32 ms (84.8%), Space: 92 MB (29.47%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+
+    public int amountOfTime(TreeNode root, int start) {
+        max = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> nodeToDistanceMap = new HashMap<>();
+        findDistance(root, start, nodeToDistanceMap);
+        findNodes(root, start, nodeToDistanceMap, 0, result);
+        return max;
+    }
+
+    void findNodes(TreeNode root, int target, HashMap<Integer, Integer> nodeToDistanceMap, int distance, ArrayList<Integer> result) {
+        if (root != null) {
+            if (nodeToDistanceMap.get(root.val) != null) {
+                distance = nodeToDistanceMap.get(root.val);
+            }
+            max = Math.max(max, distance);
+            findNodes(root.left, target, nodeToDistanceMap, distance + 1, result);
+            findNodes(root.right, target, nodeToDistanceMap, distance + 1, result);
+        }
+    }
+
+    int findDistance(TreeNode root, int target, HashMap<Integer, Integer> nodeToDistanceMap) {
+        if (root != null) {
+            if (root.val == target) {
+                nodeToDistanceMap.put(target, 0);
+                return 1;
+            }
+            int left = findDistance(root.left, target, nodeToDistanceMap);
+            int right = findDistance(root.right, target, nodeToDistanceMap);
+            if (left > 0) {
+                nodeToDistanceMap.put(root.val, left);
+                return left + 1;
+            } else if (right > 0) {
+                nodeToDistanceMap.put(root.val, right);
+                return right + 1;
+            }
+        }
+        return 0;
+    }
+}
