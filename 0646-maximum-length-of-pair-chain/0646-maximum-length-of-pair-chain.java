8207d107b4e331674debf954780ac3ca8d809103
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-26 13:03:29 -0400

    Time: 177 ms (6.2%), Space: 54.8 MB (5.3%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,20 +1,23 @@
 class Solution {
 
-    private int f(int ind, int prev, int[][] pairs, int n, Integer[][] dp) {
+    /**
+     * Using hashmap to cache using the keys prev+"#"+curindex is giving TLE for some reason.
+    So using integer array here.
+     */
+
+    private int recur(int ind, int prev, int[][] pairs, int n, Integer[][] dp) {
         if (ind == n) return 0;
         int take = 0, notTake = 0;
         if (dp[ind][prev + 1] != null) return dp[ind][prev + 1];
-        if (prev == -1 || pairs[prev][1] < pairs[ind][0]) take = 1 + f(ind + 1, ind, pairs, n, dp);
-        notTake = f(ind + 1, prev, pairs, n, dp);
+        if (prev == -1 || pairs[prev][1] < pairs[ind][0]) take = 1 + recur(ind + 1, ind, pairs, n, dp);
+        notTake = recur(ind + 1, prev, pairs, n, dp);
         return dp[ind][prev + 1] = Math.max(take, notTake);
     }
-
-  
 
     public int findLongestChain(int[][] pairs) {
         Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
         int n = pairs.length;
         Integer[][] dp = new Integer[n + 1][n + 1];
-        return f(0, -1, pairs, n, dp);
+        return recur(0, -1, pairs, n, dp);
     }
 }
