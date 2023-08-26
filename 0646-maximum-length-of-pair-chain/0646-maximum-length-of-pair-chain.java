409dc6c4f4b06752342de9042f5a5381d37e42d8
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-26 12:50:28 -0400

    Time: 174 ms (6.35%), Space: 54.6 MB (5.3%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,25 +1,18 @@
 class Solution {
-    private HashMap<String, Integer> map;
 
-    public int findLongestChain(int[][] pairs) {
-        int n = pairs.length;
-        if (n == 0) return 0;
-        map = new HashMap<>();
-        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
-        return recur(-1, 0, pairs, n);
+    private int f(int ind, int prev, int[][] pairs, int n, Integer[][] dp) {
+        if (ind == n) return 0;
+        int take = (int) (-1e9), notTake = (int) (-1e9);
+        if (dp[ind][prev + 1] != null) return dp[ind][prev + 1];
+        if (prev == -1 || pairs[prev][1] < pairs[ind][0]) take = 1 + f(ind + 1, ind, pairs, n, dp);
+        notTake = f(ind + 1, prev, pairs, n, dp);
+        return dp[ind][prev + 1] = Math.max(take, notTake);
     }
 
-    int recur(int prevIndex, int currentIndex, int pairs[][], int n) {
-        if (currentIndex >= n) return 0;
-        String key = currentIndex + "";
-        if (map.containsKey(key)) return map.get(key);
-        int take = 0;
-        int notake = 0;
-        if (prevIndex == -1 || pairs[prevIndex][1] < pairs[currentIndex][0]) {
-            take = 1 + recur(currentIndex, currentIndex + 1, pairs, n);
-        }
-        notake = recur(prevIndex, currentIndex + 1, pairs, n);
-        map.put(key, Math.max(take, notake));
-        return Math.max(take, notake);
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+        int n = pairs.length;
+        Integer[][] dp = new Integer[n + 1][n + 1];
+        return f(0, -1, pairs, n, dp);
     }
 }
