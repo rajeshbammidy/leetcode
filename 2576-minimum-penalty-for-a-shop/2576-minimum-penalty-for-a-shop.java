da8b8998cef2d3add232ed682ee8a400ed4af55d
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-29 15:19:05 -0400

    Time: 64 ms (5.05%), Space: 44.2 MB (68.01%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,12 +1,11 @@
 class Solution {
 
     public int bestClosingTime(String customers) {
-        System.out.println(customers.length());
         if (customers.replaceAll("N", "").isEmpty() || customers.isEmpty()) return 0;
-        System.out.println(customers.length());
         int n = customers.length();
-
+        // to find # of Y's on the left
         int leftY[] = new int[n];
+        //to find # of Y's on the right
         int rightY[] = new int[n];
         if (customers.charAt(0) == 'Y') leftY[0] = 1;
         if (customers.charAt(n - 1) == 'Y') rightY[n - 1] = 1;
@@ -25,7 +24,7 @@ class Solution {
                 rightY[i] = rightY[i + 1];
             }
         }
-        
+
         int min = Integer.MAX_VALUE;
         int hour = 0;
 
