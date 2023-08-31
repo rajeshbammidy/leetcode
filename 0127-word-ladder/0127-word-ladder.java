a1fb9f73eeef34182096ec6f31ec75f8504e7327
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-08-31 00:33:17 -0400

    Time: 132 ms (35.22%), Space: 46.5 MB (37.01%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> words = new HashSet<>();
+        HashSet<String> visited = new HashSet<>();
+        for (String word : wordList) {
+            words.add(word);
+        }
+        visited.add(beginWord);
+        if (!words.contains(endWord)) return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int distance = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            distance++;
+            //System.out.println(queue);
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                if (cur.equals(endWord)) return distance;
+                StringBuilder sb = new StringBuilder(cur);
+                for (int c = 0; c < cur.length(); c++) {
+                    char orgCh = cur.charAt(c);
+                    for (int j = 0; j < 26; j++) {
+                        char ch = (char) ('a' + j);
+                        sb.replace(c, c + 1, ch + "");
+                      //  System.out.println(sb.toString());
+                        if (words.contains(sb.toString()) && !visited.contains(sb.toString())) {
+                            visited.add(sb.toString());
+                            queue.add(sb.toString());
+                        }
+                    }
+                    sb.replace(c, c + 1, orgCh + "");
+                }
+            }
+        }
+        return 0;
+    }
+}
