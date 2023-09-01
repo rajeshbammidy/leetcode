ac90f8146486c0fc1b7681a720225663488e711e
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-01 10:41:00 -0400

    Time: 2 ms (73.73%), Space: 46.9 MB (9.85%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int[] countBits(int n) {
+        int dp[] = new int[n + 1];
+        if (n == 0) {
+            dp[0] = 0;
+            return dp;
+        }
+        if (n == 1) {
+            dp[0] = 0;
+            dp[1] = 1;
+            return dp;
+        }
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            if ((i & 1) == 0) {
+                dp[i] = dp[i / 2];
+            } else {
+                dp[i] = dp[i / 2] + 1;
+            }
+        }
+        return dp;
+    }
+}
