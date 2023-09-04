122d7cbc7c9fe28c68b4ff7b82c863a97db63308
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-03 20:04:10 -0400

    Time: 32 ms (16.92%), Space: 47.3 MB (43.54%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    static class Node {
+        int v;
+        int wt;
+
+        Node(int v, int wt) {
+            this.v = v;
+            this.wt = wt;
+        }
+    }
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int distance[] = new int[n];
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int m = times.length;
+        for (int i = 0; i < m; i++) {
+            int u = times[i][0] - 1;
+            int v = times[i][1] - 1;
+            int wt = times[i][2];
+            graph.get(u).add(new Node(v, wt));
+        }
+        System.out.println(graph);
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[k - 1] = 0;
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        minHeap.add(new Node(k - 1, 0));
+        while (!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
+            int u = node.v;
+            int wt = node.wt;
+            for (Node nd : graph.get(u)) {
+                int v = nd.v;
+                int vwt = nd.wt;
+                if (distance[v] > (wt + vwt)) {
+                    distance[v] = (wt + vwt);
+                    minHeap.add(new Node(v, distance[v]));
+                }
+            }
+        }
+        System.out.println(Arrays.toString(distance));
+        int max = 0;
+        for (int x : distance) {
+            max = Math.max(max, x);
+            if (max == Integer.MAX_VALUE) return -1;
+        }
+        return max;
+    }
+}
