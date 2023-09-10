30ec2300c0ee5f5d38592800dac43f2d5e349fe7
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-10 18:30:34 -0400

    Time: 0 ms (100%), Space: 39 MB (81.44%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.java
@@ -1,5 +1,13 @@
 class Solution {
 
+    /**
+    * If there are n order there will be 2n slots
+    * lets say we have n=2, which means we have P1,P2,D1,D2;
+    and we have fours slots _,_,_,_ so lets take P1,D1 and arrange them in 4 slots
+    P1 can be arrange in 4 ways and D1 can be arranged in 3 ways in total 4*3 ways,but 
+    in half of the ways D1 would always come before P1 so we do 4*3/2 which will give valid arrangements.
+    * */
+
     public int countOrders(int n) {
         long slots = n * 2;
         long res = 1;
