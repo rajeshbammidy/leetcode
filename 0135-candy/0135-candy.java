3c6ede0f15385159fdd59f6e92d4c5c84dc39fda
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-12 23:30:40 -0400

    Time: 14 ms (16.09%), Space: 45.4 MB (5.82%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,40 @@
-class Solution {
-     public int candy(int[] ratings) {
-        int candies[] = new int[ratings.length];
-        Arrays.fill(candies, 1);
-        int sum = candies.length;
-        for (int i = 1; i < candies.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                sum -= candies[i];
-                sum += (candies[i - 1] + 1);
-                candies[i] = candies[i - 1] + 1;
-            }
-        }
+    class Solution {
+        public int candy(int[] ratings) {
+            int n = ratings.length;
+            int dp[] = new int[n];
+            Arrays.fill(dp, 1);
+            for (int i = 0; i < n; i++) {
+                int leftv = 0, rightv = 0;
 
-        for (int i = candies.length - 1; i > 0; i--) {
-            if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
-                sum -= candies[i - 1];
-                sum += (candies[i] + 1);
-                candies[i-1] = (candies[i] + 1);
-            }
-        }
-        return sum;
+                if ((i - 1) < 0 || ratings[i - 1] >= ratings[i]) {
+                    leftv = Integer.MIN_VALUE;
+                } else {
+                    leftv = dp[i - 1];
+                }
 
+
+                if ((i + 1) == n || ratings[i + 1] >= ratings[i]) {
+                    rightv = Integer.MIN_VALUE;
+                } else {
+                    rightv = dp[i + 1];
+                }
+
+                leftv = Math.max(leftv, rightv);
+                if (leftv != Integer.MIN_VALUE)
+                    dp[i] = leftv + 1;
+            }
+
+            for (int i = n - 2; i >=0; i--) {
+                if ((i-1>=0) && ratings[i - 1] < ratings[i] && dp[i - 1] >= dp[i]) {
+                    dp[i] = dp[i - 1] + 1;
+                }
+                if (ratings[i + 1] < ratings[i] && dp[i + 1] >= dp[i]) {
+                    dp[i] = dp[i + 1] + 1;
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+            int sum = 0;
+            for (int x : dp) sum += x;
+            return sum;
+        }
     }
-}
