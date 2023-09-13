ec8271ca8d35227a21f5c172a3af4bdd6587d951
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-13 15:02:57 -0400

    Time: 49 ms (38.44%), Space: 43.7 MB (96.35%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -3,14 +3,17 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         HashMap<Integer, Integer> map = new HashMap<>();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
+        //get the frequencies
         for (int x : hand) {
             map.putIfAbsent(x, 0);
             map.put(x, map.get(x) + 1);
         }
+        //add the keys into minHeap
         minHeap.addAll(map.keySet());
+
         while (!minHeap.isEmpty()) {
             int count = 0;
+            //Deque to check the consectiveness
             Deque<Integer> queue = new ArrayDeque<>();
             while (queue.size() != groupSize && !minHeap.isEmpty()) {
                 int element = minHeap.poll();
@@ -21,6 +24,7 @@ class Solution {
             if (queue.size() != groupSize) {
                 return false;
             }
+            //add the elements back into minHeap
             for (int x : queue) {
                 if (map.get(x) > 0) minHeap.add(x);
             }
