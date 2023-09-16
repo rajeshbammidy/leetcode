6a4d5c9c6d868470fd3cd0ddb9310a84f3ac116d
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-16 17:58:43 -0400

    Time: 41 ms (61.64%), Space: 44 MB (21.58%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.java
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -1,32 +1,8 @@
 class Solution {
-
-    boolean isValid(int j, int i, String words[]) {
-        String delString = words[i];
-        String prevString = words[j];
-        for (int k = 0; k < delString.length(); k++) {
-            if (prevString.equals(delString.substring(0, k) + delString.substring(k + 1))) return true;
-        }
-        return false;
-    }
-
-    // public int longestStrChain(String[] words) {
-    //     int n = words.length;
-    //     int dp[] = new int[n];
-    //     int lsc = 1;
-    //     Arrays.sort(words, (a, b) -> a.length() - b.length());
-    //     Arrays.fill(dp, 1);
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = 0; j < i; j++) {
-    //             if (words[i].length() - words[j].length() > 1) continue;
-    //             if (isValid(j, i, words) && (dp[j] + 1) > dp[i]) {
-    //                 dp[i] = dp[j] + 1;
-    //                 lsc = Math.max(lsc, dp[i]);
-    //             }
-    //         }
-    //     }
-    //     return lsc;
-    // }
-
+    /***
+        1.Sorting is required to keep all the shorter length string front
+        2. We dont have to traverse j to i, instead we can iterate the string and check.
+     */
     public int longestStrChain(String[] words) {
         int n = words.length;
         int lsc = 1;
