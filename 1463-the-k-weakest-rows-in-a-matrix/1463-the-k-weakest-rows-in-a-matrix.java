8ce4605d3d23a53c9b4c3d6b7ce3f4656765133c
Rajesh Bammidi <31992463+rajeshbammidy@users.noreply.github.com>
2023-09-18 11:56:15 -0400

    Time: 2 ms (80.96%), Space: 44.3 MB (86.29%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,32 +1,31 @@
 class Solution {
 
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->{
-            if(a[1]==b[1]){
-                return a[0]-b[0];
-            }else{
-                return a[1]-b[1];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[1] == b[1]) {
+                    return a[0] - b[0];
+                } else {
+                    return a[1] - b[1];
+                }
             }
-        });
-        int n= mat.length;
-        int m=mat[0].length;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1){
+        );
+        int n = mat.length;
+        int m = mat[0].length;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
                     count++;
                 }
             }
-            minHeap.add(new int[]{i,count});
+            minHeap.add(new int[] { i, count });
         }
-        int res[]= new int[k];
-        int i=0;
-        while(i<k){
-            res[i++]=minHeap.poll()[0];
-
+        int res[] = new int[k];
+        int i = 0;
+        while (i < k) {
+            res[i++] = minHeap.poll()[0];
         }
         return res;
-
-        
     }
 }
